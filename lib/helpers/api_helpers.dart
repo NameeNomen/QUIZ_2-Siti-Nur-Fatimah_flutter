a1046e7125e89ec6c5d2ccca8eab206f6b875b8b
NameeNomen <fat.lp3i33@gmail.com>
2025-12-11 20:17:53 +0700

    alhamdulillah kayanya hidayahnya lagi nyangkutdulu deh, mari berpikir sejenak

--- lib/helpers/api_helpers.dart
+++ lib/helpers/api_helpers.dart
@@ -21,16 +21,39 @@ Future<User> registerUser({
     print('sedang mengirim data registerasi, Harap sabar');
     var reaksi = await fatimah.post(
       url,
-    header: _header,
+    headers: _header,
     body:jsonEncode({
       'firstName': firstName,
       'lastName':lastName,
       'age':age,
       'email':email
     }),
-    )
+    );
+    if (reaksi.statusCode==200 || reaksi.statusCode ==2001) {
+      Map<String, dynamic> json = jsonDecode(reaksi.body);
+      return User.fromMap(json);
+    }else{
+      throw Exception("Registrasi gagal. Status code: ${reaksi.statusCode}");
+    }
   } catch (e) {
+      throw Exception("gagal ngepost $e");
     
   }
 }
+Future<List<Todo>> AmbilDataTodo()async{
+  var url=Uri.parse('$URL/todos');
+  try {
+     var reaksi = await fatimah.get(url); 
+     if (reaksi.statusCode==200) {
+       final data =jsonDecode(reaksi.body);
+       final List<dynamic> todosJson = data['todos'];
+       return todosJson.map((json)=>Todo.fromMap(json)).toList();
+     }else{
+      throw Exception("sabar yah ttodonya gagal di muat Status code:${reaksi.statusCode}");
+     }
+  } catch (e) {
+    throw Exception("apalagi ini getnya gagal memuat (Todos)$e");
+    }
+}
+Future<Todo
 }
